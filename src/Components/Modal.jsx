aa8830d6233989c8eb86f021837dd58b06ad5556
Cheikh-Ahmed-Tidiane-Gueye <cheikhahmedtidiane219@gmail.com>
2024-06-25 14:24:56 +0000

    Mis en place du CRUD de l'utilisateur avec Firebase (Firestore)

--- src/Components/Modal.jsx
+++ src/Components/Modal.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { FaTimes } from "react-icons/fa";
 import FormField from "./FormField";
 
 export default function Modal({ isOpen, onClose, onAddUser, selectedUser }) {
